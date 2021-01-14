7906e57c4fc221d154db3556bcff2c6363f73764
FRANCESC FERRER <francesc.ferrer01@estudiant.upf.edu>
2021-01-14 16:13:06 +0000

    Ejercicio 1 y 2

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 
+  /***************EJERCICIO 1***************/
+
   int* pa = malloc(10 * sizeof(int));
+  char* pch = malloc(10);
 
-
-
-
+  for(int i = 0; i<9; i++){
+    pa[i] = 2*i + 1;
+    printf("\n%i", pa[i]);
+  }
 
   return 0;
 }
